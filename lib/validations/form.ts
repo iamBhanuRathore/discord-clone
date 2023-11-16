148dab13dbdca89ceff309f4a6608e95dcd56d9d
bhanuo07 <rathoreb853@gmail.com>
2023-11-16 18:05:24 +0530

    .

--- lib/validations/form.ts
+++ lib/validations/form.ts
@@ -13,6 +13,6 @@ export const createChannelFormSchema = z.object({
     .refine((name) => name.toLowerCase().trim() !== "general", {
       message: "Channel name cannot be 'general'",
     }),
-  // It is to verify that the [type] can be the type of Channel Type
+  // It is to verify that the [type] should be the type of Channel Type
   type: z.nativeEnum(ChannelType),
 });
